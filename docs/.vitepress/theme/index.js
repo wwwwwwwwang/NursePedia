fae325935a3efd94f3763c882e7529d080b90fa1
king <1825153451@qq.com>
2025-05-06 16:50:23 +0800

    fix: 仅在客户端应用 Naive UI 以修复构建错误

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -5,22 +5,25 @@ import './styles/override.css'
 
 const theme = {
   ...DefaultTheme,
-  enhanceApp({ app }) {
-    // 1. 安装 Naïve UI
-    const naive = create({
-      themeOverrides: {
-        common: { primaryColor: '#3eaf7c' }
-      }
-    })
-    app.use(naive)
-    
-    // 2. 全局注册常用组件
-    app.component('NButton', NButton)
-    app.component('NLayout', NLayout)
-    app.component('NLayoutHeader', NLayoutHeader)
-    app.component('NLayoutContent', NLayoutContent)
-    app.component('NLayoutFooter', NLayoutFooter)
-    app.component('NSwitch', NSwitch)
+  enhanceApp({ app, router, siteData }) {
+    // 仅在客户端应用 Naive UI
+    if (!import.meta.env.SSR) {
+      // 1. 安装 Naïve UI
+      const naive = create({
+        themeOverrides: {
+          common: { primaryColor: '#3eaf7c' }
+        }
+      })
+      app.use(naive)
+      
+      // 2. 全局注册常用组件 (也可以按需在组件内部导入)
+      app.component('NButton', NButton)
+      app.component('NLayout', NLayout)
+      app.component('NLayoutHeader', NLayoutHeader)
+      app.component('NLayoutContent', NLayoutContent)
+      app.component('NLayoutFooter', NLayoutFooter)
+      app.component('NSwitch', NSwitch)
+    }
   }
 }
 
